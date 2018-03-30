11982620b634639983d060314243e079b6e3c8a2
Kamil Tustanowski <kamil.tustanowski@gmail.com>
2018-03-30 09:24:57 +0200

    Initial Commit

---
+++ DeeplinkPOC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeeplinkPOC
+//
+//  Created by Kamil Tustanowski on 30.03.2018.
+//  Copyright © 2018 ktustanowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
