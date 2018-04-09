f4e8d8263322d977784ea6bf0ad785633199e695
Kamil Tustanowski <kamil.tustanowski@gmail.com>
2018-04-09 14:27:50 +0200

    Cleanup

--- Playground.playground/Pages/Reactive.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Reactive.xcplaygroundpage/Contents.swift
@@ -13,55 +13,6 @@ func dispatchAfter(_ delay: Double, closure: @escaping () -> ()) {
     }
 }
 
-//let homeDataProducer = SignalProducer<Bool, NoError> {observer, _ in
-//    dispatchAfter(1.0) {
-//        observer.send(value: true)
-//        observer.sendCompleted()
-//    }
-//}//.logEvents(identifier: "HP")
-//
-//let newRequirementProducer = SignalProducer<Bool, NoError> {observer, _ in
-//    dispatchAfter(3.0) {
-//        observer.send(value: false)
-//        observer.sendCompleted()
-//    }
-//    }//.logEvents(identifier: "NEW")
-//
-//let newestRequirementProducer = SignalProducer<Bool, NoError> {observer, _ in
-//    dispatchAfter(3.5) {
-//        observer.send(value: false)
-//        observer.sendCompleted()
-//    }
-//    }//.logEvents(identifier: "NEWEST")
-//
-//let universalLinkActivity = NSUserActivity(activityType: NSUserActivityTypeBrowsingWeb)
-//
-//let linkParserProducer = LinkFactory.make(with: URL(string: "http://www.o2.pl?sso=go")!)
-//    .skipNil()
-//    .take(first: 1)
-//    .logEvents(identifier: "LPP")
-//
-//
-//// UL LinkFactory.make(with: universalLinkActivity)
-//// URL LinkFactory.make(with: URL(string: "http://www.o2.pl")!)
-//
-//let x = linkParserProducer.flatMap(.latest) { link in
-//    SignalProducer.zip(homeDataProducer,
-//                       SingleSignOn.login(using: link),
-//                       newRequirementProducer,
-//                       newestRequirementProducer,
-//                       SignalProducer.init(value: link))
-//    }.startWithResult { result in
-//        switch result {
-//        case .success(let (isHomeLoaded, ssoLoginStatus, newFeature, newestFeature, link)):
-//            print("GOT: home loaded: \(isHomeLoaded), sso authenticated: \(ssoLoginStatus), new requirement: \(newFeature), newest requirement: \(newestFeature) link: \(link)")
-//        //TODO: Handle appropriately
-//        case .failure(let error):
-//            print("GOT: \(error)")
-//        }
-//        //TODO: start navigation here based on outcome
-//}
-
 struct LinkerDelegate: LinkDispatcherDelegate {
     func willStartLinking() {
         print("Reset to loading screen")
